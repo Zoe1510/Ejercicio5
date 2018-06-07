59314c879a804179449857c75b19289539a50924
Zoe1510 <patriciabermudez465@gmail.com>
2018-06-07 11:00:17 -0400

    first commit

---
+++ Ejercicio5_DFDaCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5_DFDaCSharp
+{
+    /*5. Ejercicio de vectores: 
+	Menú, subprogramas y vectores que realice lo siguiente:
+    1.	Sumar dos vectores
+    2.	Multiplicar dos vectores
+    3.	Multiplicar por escalar
+    4.	Definir cuál de los dos vectores es mayor, menor o igual, por medio su magnitud 
+    5.	Salir del programa
+    
+    * Al capturar el tamaño de los vectores solo se debe permitir números positivos y mayores a uno. 
+    * El tamaño lo define el usuario. 
+    
+         
+    NOTA: Se puede utilizar cualquier función necesaria de C#.
+ 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
